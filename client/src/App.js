dc81ebc99273f9d40c0586859c22cd86fd2aee88
Knock <enoch10evelyn@gmail.com>
2022-11-13 17:53:40 -0800

    mutation,query folder added with imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import EventForm from "./components/EventForm";
 import EventList from "./components/EventList";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const events = [
   {
